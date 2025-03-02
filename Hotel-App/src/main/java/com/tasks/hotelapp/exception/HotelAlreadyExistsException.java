4b630750bebacb20fb747be7b2be1af58eb291f5
Black_Daddy <sashamynzul@gmail.com>
2025-03-02 15:10:42 +0300

    feat: Reamde

--- Hotel-App/src/main/java/com/tasks/hotelapp/exception/HotelAlreadyExistsException.java
+++ Hotel-App/src/main/java/com/tasks/hotelapp/exception/HotelAlreadyExistsException.java
@@ -1,6 +1,5 @@
 package com.tasks.hotelapp.exception;
 
-import com.tasks.hotelapp.model.entity.Hotel;
 
 public class HotelAlreadyExistsException extends RuntimeException {
     public HotelAlreadyExistsException(String message){
